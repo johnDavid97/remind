18763f911b9e7bd1f64280513b7345c4c6d2438f
johnDavid97 <johankaldefoss@hotmail.com>
2025-01-09 13:01:56 +0100

    Initial Commit

---
+++ remind/remindApp.swift
@@ -0,0 +1,17 @@
+//
+//  remindApp.swift
+//  remind
+//
+//  Created by Johan Nordstrand on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct remindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
