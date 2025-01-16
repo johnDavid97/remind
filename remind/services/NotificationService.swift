6c937b1aa4f62500cf75e82e160fb813570c699b
johnDavid97 <johankaldefoss@hotmail.com>
2025-01-16 13:07:43 +0100

    Added a propper dateservice and connected it to the NotficationService fo the notification to trigger at a spesific date/time

---
+++ remind/services/NotificationService.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationService.swift
+//  remind
+//
+//  Created by Johan Nordstrand on 14/01/2025.
+//
+
+import Foundation
---
+++ remind/viewComponent/comps/CalenderSelectorComp.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderSelectorComp.swift
+//  remind
+//
+//  Created by Johan Nordstrand on 16/01/2025.
+//
+
+import SwiftUI
+
+struct CalenderSelectorComp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderSelectorComp()
+}
